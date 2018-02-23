f52348d44db6fb988c864a55511a53627bc00753
Isaac Andersen <ila4@login-teer-07.oit.duke.edu>
2018-02-22 22:20:34 -0500

    added func decs

--- p1t/thread.cc
+++ p1t/thread.cc
@@ -1,6 +1,9 @@
 #include "thread.h"
 #include <ucontext.h>
 
+
+// Notes
+
 // /*
 // * Initialize a context structure by copying the current thread's context.
 // */
@@ -20,3 +23,38 @@
 // * Direct the new thread to start by calling start(arg1, arg2).
 // */
 // makecontext(ucontext_ptr, (void (*)()) start, 2, arg1, arg2);
+
+int thread_libinit(thread_startfunc_t func, void *arg) {
+
+}
+
+int thread_create(thread_startfunc_t func, void *arg) {
+
+}
+
+int thread_yield(void) {
+
+}
+
+
+/*///////////////////////  ^^ FINISH THESE FIRST ^^ ////////////////////////*/
+
+int thread_lock(unsigned int lock) {
+
+}
+
+int thread_unlock(unsigned int lock) {
+
+}
+
+int thread_wait(unsigned int lock, unsigned int cond) {
+
+}
+
+int thread_signal(unsigned int lock, unsigned int cond) {
+
+}
+
+int thread_broadcast(unsigned int lock, unsigned int cond) {
+
+}
