6c1d30227e28fde143bde80b3ebd56a925363f45
Isaac Andersen <ila4@login-teer-02.oit.duke.edu>
2018-02-25 14:16:08 -0500

    completed thread-create (to our knowledge.)

--- p1t/thread.cc
+++ p1t/thread.cc
@@ -6,7 +6,7 @@
 struct thread_t {
 	ucontext_t* 	context;
 	char*			stack;
-	bool			finished;
+	bool			done;
 }
 
 static thread_t* active_thread;
@@ -17,52 +17,89 @@ static queue<thread_t> blocked;
 
 static bool libinit_completed = false;
 
-int thread_libinit(thread_startfunc_t func, void *arg) {
+extern int thread_libinit(thread_startfunc_t func, void *arg) {
 	interrupt_disable();
 
 	if( libinit_completed ) {
 		interrupt_enable();
-		return 1; // Unsuccessful
+		return 1; // tries to reinitialize libinit
+	} else {
+		libinit_completed = true;
 	}
 
 	// create new thread. 
+	interrupt_enable();
 	thread_create(func, arg);
+	interrupt_disable();
 
+	active_thread = ready.pop_front();
 
-	libinit_completed = true;
+	setcontext(active_thread->context); // do we set or do we swap context? 
 
+	// while there are threads to run, run them....
+	while(!ready.empty()) {
+		if (active_thread->done) {
+			delete_thread(active_thread);
+		}
+
+	}
+	// Clean up all our memmory. 
+
+	cout << "Thread library exiting.\n";
 	interrupt_enable();
 	return 0; 
 }
 
-int thread_create(thread_startfunc_t func, void *arg) {
+extern int thread_create(thread_startfunc_t func, void *arg) {
+	interrupt_disable();
 
-	this_thread = (thread_t*) malloc(sizeof(thread_t));
+	if ( !libinit_completed ) {
+		interrupt_enable();
+		return 1; // error: 1 - libinit not called successfully
+	}
+
+	thread_t new_thread = new thread_t;
+	new_thread->context = new ucontext_t;
+	new_thread->stack 	= new char[STACK_SIZE];
+	new_thread->done	= false;
 
 	/* ---------------- Create a new context ---------------- */
 
-	if (!getcontext(newthread->context)) {
-		// error handling
+	if (!getcontext(new_thread->context)) {
+		interrupt_enable();
+		return 1;
 	}
 
-	ucontext_t* ucontext_ptr = newthread;
+	/* -------------------Configure context ------------------------ */
+	new_thread->context->uc_stack.ss_sp 	= new_thread->stack;
+	new_thread->context->uc_stack.ss_size 	= STACK_SIZE;
+	new_thread->context->uc_stack.ss_flags 	= 0;
+	new_thread->context->uc_link 			= NULL;
 
-	char *stack = new char [STACK_SIZE];
-	ucontext_ptr->uc_stack.ss_sp = stack;
-	ucontext_ptr->uc_stack.ss_size = STACK_SIZE;
-	ucontext_ptr->uc_stack.ss_flags = 0;
-	ucontext_ptr->uc_link = NULL;
 
-	makecontext(ucontext_ptr, func, 1, arg);
+	/* ---------------- Deliver function to context ---------------- */
+	makecontext(new_thread->context, run_stub, 2, func, arg);
 
+	ready.push_back(new_thread);
+	interrupt_enable();
+	return 0;
+}
+
+int delete_thread(thread_t t) {
+	char* stack = t->stack;
+	delete(t->context);
+	delete(t);
+	delete(stack);
+}
+
+int run_stub(thread_startfunc_t func, void *arg) {
 	interrupt_enable();
 	func(arg);
 	interrupt_disable();
-
-	// destroy thread, update state of thread lib. 
+	active_thread->done = true;
 }
 
-int thread_yield(void) {
+extern int thread_yield(void) {
 	// save current context => move to end of ready.
 	// pop head from ready => set to active
 	// new set context
@@ -70,22 +107,22 @@ int thread_yield(void) {
 
 /*///////////////////////  ^^ FINISH THESE FIRST ^^ ////////////////////////*/
 
-int thread_lock(unsigned int lock) {
+extern int thread_lock(unsigned int lock) {
 
 }
 
-int thread_unlock(unsigned int lock) {
+extern int thread_unlock(unsigned int lock) {
 
 }
 
-int thread_wait(unsigned int lock, unsigned int cond) {
+extern int thread_wait(unsigned int lock, unsigned int cond) {
 
 }
 
-int thread_signal(unsigned int lock, unsigned int cond) {
+extern int thread_signal(unsigned int lock, unsigned int cond) {
 
 }
 
-int thread_broadcast(unsigned int lock, unsigned int cond) {
+extern int thread_broadcast(unsigned int lock, unsigned int cond) {
 
 }
