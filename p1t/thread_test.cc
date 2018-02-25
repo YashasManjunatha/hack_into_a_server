12293840cdfb13701af522ce2b4feb9b28ae988b
Jason Wang <jw502@login-teer-02.oit.duke.edu>
2018-02-25 16:27:31 -0500

    added thread_test.cc

---
+++ p1t/thread_test.cc
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+#include <iostream>
+#include "thread.h"
+#include <assert.h>
+
+using namespace std;
+
+int g=0;
+
+void loop(void *a) {
+  char *id;
+  int i;
+
+  id = (char *) a;
+  cout << "loop called with id " << (char *) id << endl;
+
+  for (i=0; i<5; i++, g++) {
+    cout << id << ":\t" << i << "\t" << g << endl;
+    if (thread_yield()) {
+      cout << "thread_yield failed\n";
+      exit(1);
+    }
+  }
+}
+
+/* ----------------------------- CASE 1 TEST ----------------------------- */
+// Tests to see if thread_create works and if thread will yield to itself
+// If it is the only thing in in the active and ready queues
+void case1(void *a) {
+  int arg;
+  arg = (long int) a;
+
+  cout << "case1 called with arg " << arg << endl;
+  if (thread_create((thread_startfunc_t) loop, (void *) "child thread")) {
+    cout << "thread_create failed\n";
+    exit(1);
+  }
+}
+
+/* ----------------------------- CASE 2 TEST ----------------------------- */
+// Same as case1, except tests to see if the parent thread can loop after
+// thread_create's child thread
+void case2(void *a) {
+  int arg;
+  arg = (long int) a;
+
+  cout << "case2 called with arg " << arg << endl;
+  if (thread_create((thread_startfunc_t) loop, (void *) "child thread")) {
+    cout << "thread_create failed\n";
+    exit(1);
+  }
+
+  loop( (void *) "parent thread");
+}
+
+/* ----------------------------- CASE 3 TEST ----------------------------- */
+// Creates numLoops number of threads that run the loop function
+void case3(void *a) {
+  int arg;
+  arg = (long int) a;
+
+  cout << "case3 called with arg " << arg << endl;
+  int i = 0;
+  while( i < 7 ){
+  	if (thread_create((thread_startfunc_t) loop, (void *) "child thread")) {
+	    cout << "thread_create failed\n";
+	    exit(1);
+	  }
+  }
+}
+
+/* ----------------------------- MAIN FUNCTION ----------------------------- */
+int main(int argc, char *argv[]) {
+  if( argc < 2 ){
+  	cout << "Input must select at least one test\n";
+  	exit(1);
+  }
+  int testChoice = (int) *argv[1];
+  switch( testChoice ){
+  	case 1:
+  		if (thread_libinit( (thread_startfunc_t) case1, (void *) 1)) {
+		    cout << "thread_libinit failed\n";
+		    exit(1);
+		  }
+		  break;
+	  case 2:
+	  	if (thread_libinit( (thread_startfunc_t) case2, (void *) 2)) {
+		    cout << "thread_libinit failed\n";
+		    exit(1);
+		  }
+	  	break;
+	  case 3:
+	  	if (thread_libinit( (thread_startfunc_t) case3, (void *) 3)) {
+		    cout << "thread_libinit failed\n";
+		    exit(1);
+		  }
+	  	break;
+  }
+  exit(0);
+}
